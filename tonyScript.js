eebc0f30e0a7063125a90106bf3fb4d795774512
unknown <tonycruz417@gmail.com>
2019-05-15 23:54:16 -0500

    added new info

--- tonyScript.js
+++ tonyScript.js
@@ -1,5 +1,7 @@
 var travisJordan = document.querySelector("#avgPrice");
 
 travisJordan.innerHTML = "StockX Average Price: $" + data.ProductActivity[0].localAmount;
-
-console.log(data);
+console.log(data.ProductActivity[0].shoeSize);
+for (var i = 0; i < data.ProductActivity.length; i++) {
+    console.log(data.ProductActivity[i].localAmount);
+}
